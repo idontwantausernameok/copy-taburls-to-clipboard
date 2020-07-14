8039aa1e73bc40315d78fc6da688018e59f46cc8
igorlogius <67047467+igorlogius@users.noreply.github.com>
2020-07-15 00:34:23 +0200

    refactor: notification create

--- background.js
+++ background.js
@@ -1,52 +1,57 @@
 
 const extId = 'tabs2clip';
-const excluded_urls = ['chrome','moz','about','data'];
+const excluded_urls = ['chrome:','moz:','about:','data:','blob:'];
 
-function onBrowserActionClicked() { 
+async function onBrowserActionClicked() { 
 
-	// query all tabs from active Window
-	browser.tabs.query({currentWindow: true}).then( (tabs) => {
+	let notify_title = '';
+	let notify_message = '';
+
+	try {
+
+		// query all tabs from active Window
+		const tabs = await browser.tabs.query({currentWindow: true});
 
 		// url storage
 		let clipText = '';
-		
+
 		// get urls from tabs
 		tabs.forEach( (tab) => {
 			for (let ex of excluded_urls) {
-				if( tab.url.startsWith(ex) ){
-					return;
-				}
+				if( tab.url.startsWith(ex) ){ return; }
 			}
 			clipText += tab.url + '\n';
 		});
 		// remove trailing \n
 		clipText = clipText.trim();
 
+		// special case for empty list, lets inform the user
+		if( clipText === '' ) {
+			throw Error('url list empty, please note, that urls starting with "'+ excluded_urls.join('", "') + '" will be ignored');
+		}
+
+
 		// write urls to clipboard
-		navigator.clipboard.writeText(clipText).then(function() {
+		await navigator.clipboard.writeText(clipText);
 
-			browser.notifications.create(extId, {
-				"type": "basic",
-				"iconUrl": browser.runtime.getURL("icons.png"),
-				"title": "Successfully copied tab urls to clipboard",
-				"message": "Use CTRL+V <PASTE> to insert them into any notepad or editor"
-			});
+		notify_title = "Successfully copied urls to clipboard";
+		notify_message = "Use CTRL+V <PASTE> to insert them into any notepad or editor";
 
-		});
 
-	}).catch( (e) => {
 
-		browser.notifications.create(extId, {
-			"type": "basic",
-			"iconUrl": browser.runtime.getURL("icons.png"),
-			"title": "Failed to copy tab urls to clipboard",
-			"message": e.toString()
-		});
+	} catch(e) {
+		notify_title = 'Failed to copy urls to clipboard';
+		notify_message = e.message;
+	}
 
+	browser.notifications.create(extId, {
+		"type": "basic",
+		"iconUrl": browser.runtime.getURL("icons.png"),
+		"title": notify_title, 
+		"message":  notify_message 
 	});
-
 }
 
-// listener
+// register listener
 browser.browserAction.onClicked.addListener(onBrowserActionClicked); 
 
